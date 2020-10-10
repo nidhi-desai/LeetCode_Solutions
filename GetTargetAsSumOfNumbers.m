c15d213487281dd3abbb87dbe2db8bb752ef6a4d
nidhi-desai <50000288+nidhi-desai@users.noreply.github.com>
2020-10-09 19:33:27 -0700

    Add files via upload

---
+++ GetTargetAsSumOfNumbers.m
@@ -0,0 +1,24 @@
+function GetTargetAsSumOfNumbers(target)
+
+% Add (insert) the mathematical operators + or - (plus or minus) before any
+% of the digits in the decimal numeric string "123456789" such that the 
+% resulting mathematical expression adds up to a particular target sum.
+% Example: 123 + 4 - 5 + 67 - 89 = 100
+
+num = '123456789';
+target = 100;
+% 1=plus, -1=minus, 0=nothing
+soln = zeros(8,1);
+for w = 8:-1:1
+    if sum(1:w)+(w+1) == target
+        soln(w) = 1;
+    elseif sum(1:w)-(w+1) == target 
+        soln(w) = -1;
+    else
+        soln(w) = 0;
+    
+        
+    
+    end
+end
+end
